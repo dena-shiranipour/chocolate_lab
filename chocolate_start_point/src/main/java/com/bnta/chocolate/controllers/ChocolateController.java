62c484fa87f83fe12ea69af555d66a2a84392bf4
Dena Shiranipour <denashirani@gmail.com>
2024-06-04 16:44:01 +0100

    first extension completed

--- chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/chocolates")
@@ -22,6 +24,17 @@ public class ChocolateController {
     public ResponseEntity<List<Chocolate>> getAllChocolate(){
         List<Chocolate> chocolateList = chocolateService.getAllChocolate();
         return new ResponseEntity<>(chocolateList, HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Chocolate> getChocolateById(@PathVariable Long id){
+        Optional<Chocolate> chocolate = chocolateService.getChocolateById(id);
+        if(chocolate.isPresent()){
+            return new ResponseEntity<>(chocolate.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/EstateController.java
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/EstateController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/estates")
@@ -24,6 +26,17 @@ public class EstateController {
     public ResponseEntity<List<Estate>> getAllEstates(){
         List<Estate> estateList = estateService.getAllEstates();
         return new ResponseEntity<>(estateList, HttpStatus.OK);
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Estate> getEstateById(@PathVariable Long id){
+        Optional<Estate> estate = estateService.getEstateById(id);
+        if(estate.isPresent()){
+            return new ResponseEntity<>(estate.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 
 }
